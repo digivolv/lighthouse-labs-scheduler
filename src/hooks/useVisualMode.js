a4ec4734f6fa110cc0f92354a1fbb438f54d4e3f
Edwin Ly <edwinjly@gmail.com>
2022-02-25 20:46:33 -0500

    Add useVisualMode function

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initialMode) {
+  const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
+
+  const transition = (newMode, replace = false) => {
+    if (!replace) {
+      setMode(newMode);
+      setHistory([...history, newMode]);
+    } else {
+      setMode(newMode);
+      //spread injects the values into arrray
+      //no spread will inject array into array
+      setHistory((prev) => [...prev.slice(0, prev.length - 1), newMode]);
+      // setHistory([newMode]);
+    }
+  };
+
+  const back = () => {
+    if (history.length > 1) {
+      //asynch so have to store in variable
+      const prevHistory = history.slice(0, -1);
+      setHistory(prevHistory);
+      setMode(prevHistory[prevHistory.length - 1]);
+    }
+  };
+
+  return { mode, transition, back };
+}
