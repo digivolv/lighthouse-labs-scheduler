5d828901fca1f9dfe830d7b3e2a4b52d9703b800
Edwin Ly <edwinjly@gmail.com>
2022-03-02 17:52:36 -0500

    Cypress tests pass

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -33,8 +33,8 @@ describe("Appointments", () => {
 
     cy.contains("Confirm").click();
 
-    cy.contains("Deleting").should("exist");
-    cy.contains("Deleting").should("not.exist");
+    cy.contains("DELETING").should("exist");
+    cy.contains("DELETING").should("not.exist");
 
     cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
   });
